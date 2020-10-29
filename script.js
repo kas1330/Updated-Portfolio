d9133e4c5081a77c3caf1149c0513464448ced91
Kerri Sexton <k.sexton804@gmail.com>
2020-10-29 17:23:02 -0400

    JS file now works and changes elements on the page based on how large the window is

---
+++ script.js
@@ -0,0 +1,26 @@
+// var header = $('#topHeader');
+var header = document.getElementById('topHeader');
+var header2 = document.getElementById('topHeader2');
+var navText = document.getElementById('myName');
+
+// var navText = $('#myName');
+$(window).resize(function() {
+    var height = window.innerHeight;
+    var width = window.innerWidth;
+
+    console.log('Height: ', height, 'Width: ', width);
+
+    if(width <= 995){
+        header.classList.add('hidden');
+        header2.classList.add('hidden');
+
+        console.log('Inside if statement.');
+        navText.classList.remove('hidden');
+    }
+    else{
+        header.classList.remove('hidden');
+        header2.classList.remove('hidden');
+        navText.classList.add('hidden');
+
+    }
+  });
