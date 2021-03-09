f63375824815cc37a5f2e81da7c6f4380c9fdb96
Kerri Sexton <k.sexton804@gmail.com>
2021-03-08 21:30:27 -0500

    got rid of console logs

--- script.js
+++ script.js
@@ -1,46 +1,34 @@
-var header = document.getElementById('topHeader');
-var header2 = document.getElementById('topHeader2');
-var navText = document.getElementById('myName');
-
-
+var header = document.getElementById("topHeader");
+var header2 = document.getElementById("topHeader2");
+var navText = document.getElementById("myName");
 
 var height = window.innerHeight;
 var width = window.innerWidth;
 
-console.log('Height: ', height, 'Width: ', width);
+// console.log('Height: ', height, 'Width: ', width);
 
-
-if(width <= 995){
-    header.classList.add('hidden');
-    header2.classList.add('hidden');
-
-    console.log('Inside if statement.');
-    navText.classList.remove('hidden');
-}
-else{
-    header.classList.remove('hidden');
-    header2.classList.remove('hidden');
-    navText.classList.add('hidden');
-
+if (width <= 995) {
+  header.classList.add("hidden");
+  header2.classList.add("hidden");
+  navText.classList.remove("hidden");
+} else {
+  header.classList.remove("hidden");
+  header2.classList.remove("hidden");
+  navText.classList.add("hidden");
 }
 
-$(window).resize(function() {
-    var height = window.innerHeight;
-    var width = window.innerWidth;
+$(window).resize(function () {
+  var height = window.innerHeight;
+  var width = window.innerWidth;
 
-    console.log('Height: ', height, 'Width: ', width);
+  if (width <= 995) {
+    header.classList.add("hidden");
+    header2.classList.add("hidden");
 
-    if(width <= 995){
-        header.classList.add('hidden');
-        header2.classList.add('hidden');
-
-        console.log('Inside if statement.');
-        navText.classList.remove('hidden');
-    }
-    else{
-        header.classList.remove('hidden');
-        header2.classList.remove('hidden');
-        navText.classList.add('hidden');
-
-    }
-  });
+    navText.classList.remove("hidden");
+  } else {
+    header.classList.remove("hidden");
+    header2.classList.remove("hidden");
+    navText.classList.add("hidden");
+  }
+});
